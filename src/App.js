fc2e03bc2db261d6bdc82a113b1b7d047062a180
MardonioEng <mardoniorodrigues11@gmail.com>
2021-05-10 20:14:04 -0300

    State com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -3,14 +3,38 @@ import { Component } from "react";
 
 class App extends Component {
   state = {
-    name: "Mardônio Rodrigues",
-    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "O titulo 1",
+        body: "O corpo 1",
+      },
+      {
+        id: 2,
+        title: "O titulo 2",
+        body: "O corpo 2",
+      },
+      {
+        id: 3,
+        title: "O titulo 3",
+        body: "O corpo 3",
+      },
+    ],
   };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
